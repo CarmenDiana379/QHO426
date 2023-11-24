b276a073864b5c5b286a98a508148dc947576637
CarmenDiana379 <csatloscarmen@yahoo.com>
2023-11-24 10:09:56 +0000

    cwd

---
+++ txt_file_tasks.py
@@ -0,0 +1,15 @@
+import os
+
+
+def cwd():
+    path = os.getcwd()
+    print(f"The current working directory is {path}")
+    print("The directory contains the following files:")
+    for each_file in os.listdir(path):
+        print(each_file)
+
+def run():
+    print("precessing ...")
+    cwd()
+
+run()
